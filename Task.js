92a929e75ba9b86d753953b4fab13770359ec898
Jeffrey Field <135449242+JeffreyField123@users.noreply.github.com>
2024-01-08 21:13:42 -0600

    Add files via upload

---
+++ Task.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native'
+
+const Task = (props) => {
+
+    return (
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+                <View style={styles.square}></View>
+                <Text style={styles.itemText}>{props.text}</Text>
+            </View>
+            <View style={styles.circular}></View>
+        </View>
+
+    
+      
+    )
+    
+}
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: 'white',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 25,
+    },
+    itemLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap'
+    },
+    square: {
+        width: 25,
+        height: 50,
+        backgroundColor: 'tan',
+        opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15,
+    },
+    itemText: {
+        maxWidth: '80%'
+    },
+    circular: {
+        width: 12,
+        height: 12,
+        borderColor: 'black',
+        borderWidth: 2,
+        borderRadius: 5,
+    }
+
+});
+
+export default Task;
